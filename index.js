0adc66269746bb844a1a7a24341c474bb5e53641
Mhistersax <bamigboyepelumi38@gmail.com>
2023-04-27 22:51:52 +0100

    Source Code With Comments

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const readline = require("readline");
-const crypto = require("crypto");
+// Importing Built In Node Js Functions
+const readline = require("readline"); // Responsible For the Reading of User's Input
+const crypto = require("crypto"); //Responsible for the random generation
 
 const userData = readline.createInterface({
+  //block responsible for standard Input And OutPut
   input: process.stdin,
   output: process.stdout,
 });
 
+// Responsible for user Prompt via Terminal
 userData.question("Welome, Please Input Minimum Value: ", (min) => {
   userData.question("Input Maximum Value: ", (max) => {
-    const minNum = parseInt(min);
+    const minNum = parseInt(min); //parseInt was use to tell NODE that we dealing with int
     const maxNum = parseInt(max);
     console.log("Random number = " + random(minNum, maxNum));
     userData.close();
   });
 });
 
+//function for random generation
 function random(min, max) {
   const range = max - min + 1;
   const bytesNeeded = Math.ceil(Math.log2(range) / 8);
